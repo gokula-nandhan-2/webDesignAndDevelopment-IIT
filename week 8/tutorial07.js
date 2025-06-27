f1202873f7971beead35af778cb790425352c818
Gokula Nandhan <gokula.20233027@iit.ac.lk>
2025-06-27 08:14:19 +0530

    updated tutorial 07

--- week 8/tutorial07.js
+++ week 8/tutorial07.js
@@ -16,7 +16,7 @@ function getSelectedText(selectList){
 }
 
 
-/*
+
 document.addEventListener(onchange,CheckOptions(this.form));
 
 function CheckOptions(formRef){
@@ -36,4 +36,56 @@ function displayBouquet(bouquet){
 
 function resetOrder(){
 
-}*/
+}
+
+
+
+
+
+
+document.getElementById("bouquetOptions").addEventListener("change", function () {
+  CheckOptions(document.forms[0]);
+});
+
+document.getElementById("orderForm").addEventListener("reset", function () {
+  resetOrder();
+});
+
+function CheckOptions(formRef) {
+  let optionValue = getSelectedValue(formRef.bouquetOptions);
+
+  if (optionValue.includes("Champagne")) {
+    document.getElementById("product1").querySelector("img").src = "whiteChamp.jpg";
+    document.getElementById("product2").querySelector("img").src = "pinkChamp.jpg";
+    document.getElementById("product3").querySelector("img").src = "redChamp.jpg";
+  } else {
+    document.getElementById("product1").querySelector("img").src = "white.jpg";
+    document.getElementById("product2").querySelector("img").src = "pink.jpg";
+    document.getElementById("product3").querySelector("img").src = "red.jpg";
+  }
+
+  let chosenBouquet = getRadioValue(formRef.colour);
+  displayBouquet(chosenBouquet);
+}
+
+function displayBouquet(bouquet) {
+  let orderDiv = document.getElementById("order");
+  if (!bouquet) {
+    orderDiv.innerHTML = "";
+    return;
+  }
+
+  let selectedProduct = document.getElementById("product1");
+  if (bouquet === "pink") selectedProduct = document.getElementById("product2");
+  if (bouquet === "red") selectedProduct = document.getElementById("product3");
+
+  orderDiv.innerHTML = selectedProduct.innerHTML;
+}
+
+function resetOrder() {
+  document.getElementById("product1").querySelector("img").src = "white.jpg";
+  document.getElementById("product2").querySelector("img").src = "pink.jpg";
+  document.getElementById("product3").querySelector("img").src = "red.jpg";
+
+  document.getElementById("order").innerHTML = "";
+}
